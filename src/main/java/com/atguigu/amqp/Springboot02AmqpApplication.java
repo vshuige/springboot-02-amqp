62011c9eace187dd35ea832ec356ffddd22a9ee1
liangzhihui <1498730879@qq.com>
2018-12-05 17:24:42 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/amqp/Springboot02AmqpApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02AmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot02AmqpApplication.class, args);
+	}
+}
