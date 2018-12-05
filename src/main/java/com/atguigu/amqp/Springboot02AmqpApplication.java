8f64a697beaa4c7d7fa3c54a1d4c9d6460feac7f
liangzhihui <1498730879@qq.com>
2018-12-05 17:29:49 +0800

    修改

--- src/main/java/com/atguigu/amqp/Springboot02AmqpApplication.java
+++ src/main/java/com/atguigu/amqp/Springboot02AmqpApplication.java
@@ -6,6 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot02AmqpApplication {
 
+
+
+	/**
+	 * 自动配置
+	 *  1、RabbitAutoConfiguration
+	 *  2、有自动配置了连接工厂ConnectionFactory；
+	 *  3、RabbitProperties 封装了 RabbitMQ的配置
+	 *  4、 RabbitTemplate ：给RabbitMQ发送和接受消息；
+	 *  5、 AmqpAdmin ： RabbitMQ系统管理功能组件;
+	 *  	AmqpAdmin：创建和删除 Queue，Exchange，Binding
+	 *  6、@EnableRabbit +  @RabbitListener 监听消息队列的内容
+	 *
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot02AmqpApplication.class, args);
 	}
