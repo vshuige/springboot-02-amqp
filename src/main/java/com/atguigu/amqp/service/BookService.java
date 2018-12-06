4524e6b00bc0c796bb9d6576463c29500d9a657c
liangzhihui <1498730879@qq.com>
2018-12-06 09:36:21 +0800

    修改

---
+++ src/main/java/com/atguigu/amqp/service/BookService.java
@@ -0,0 +1,4 @@
+package com.atguigu.amqp.service;
+
+public class BookService {
+}
--- src/test/java/com/atguigu/amqp/Springboot02AmqpApplicationTests.java
+++ src/test/java/com/atguigu/amqp/Springboot02AmqpApplicationTests.java
@@ -1,16 +1,57 @@
 package com.atguigu.amqp;
 
+import com.atguigu.amqp.bean.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot02AmqpApplicationTests {
 
+
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+
+
+
+	/**
+	 * 1、单播（点对点）
+	 */
 	@Test
 	public void contextLoads() {
+		//Message需要自己构造一个;定义消息体内容和消息头
+		//rabbitTemplate.send(exchage,routeKey,message);
+
+		//object默认当成消息体，只需要传入要发送的对象，自动序列化发送给rabbitmq；
+		//rabbitTemplate.convertAndSend(exchage,routeKey,object);
+
+		HashMap<Object, Object> map = new HashMap<>();
+		map.put("msg","这是第一条消息");
+		map.put("data", Arrays.asList("laji",11,22,3,true));
+//		rabbitTemplate.convertAndSend("exchanges.direct","atguigu.news",map);
+		rabbitTemplate.convertAndSend("exchanges.direct","atguigu.news",new Book("西游记","老吴"));
+
 	}
 
+	@Test
+	public void receive(){
+		Object o = rabbitTemplate.receiveAndConvert("atguigu.news");
+		System.out.println(o.getClass());
+		System.out.println(o);
+	}
+
+	/**
+	 * 广播
+	 */
+	@Test
+	public void sendMsg(){
+		rabbitTemplate.convertAndSend("exchanges.fanout","",new Book("红楼梦","曹雪芹"));
+	}
 }
